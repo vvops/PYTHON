ecaff1273b72c2f4f6c461276906ff16bf6ffb5d
vovaops <vova234pp@gmail.com>
2025-01-21 22:53:06 +0300

    типы данных

---
+++ composite.py
@@ -0,0 +1,8 @@
+############СПИСК
+strings = ["hello", "world"]
+numbers = [1,2,3,4]
+data = ["hello", 1]
+
+print(data)
+print(numbers)
+print(strings)
---
+++ string.py
@@ -0,0 +1,20 @@
+s1 = "hello"
+s2 = 'hello'
+s3 = """hello"""
+
+####################
+
+print(s1)
+print(s2)
+print(s3)
+
+####################
+
+watch = "hello " + "world"
+print(watch)
+
+###################
+
+l = len(watch)
+print(l)
+print(len(s1))
