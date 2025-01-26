28d94fbba5afb796a08c9d4adf895f5afab203cb
vovaops <vova234pp@gmail.com>
2025-01-26 18:03:13 +0300

    урок элементы списка и словар

--- composite.py
+++ composite.py
@@ -1,6 +1,6 @@
 ############-----СПИСК----######
 strings = ["hello", "world"]
-numbers = [1,2,3,4]
+numbers = [1,2,3,4,5]
 data = ["hello", 1]
 
 print(data)
@@ -8,3 +8,20 @@ print(numbers)
 print(strings)
 
 ##############################
+
+summa = numbers + data
+print(summa)
+
+numbers.append(6)
+print(numbers)
+
+first = strings[0]
+second = strings[1]
+print(first)
+print(second)
+#print(strings[3]) ####отсутствует 3 элемент списка strings
+
+strings_length = len(strings)
+print(strings_length)
+s = sum(numbers)
+print(s)
