937e30b7c94cc7ee367cbb94438d1f385ee245ba
vovaops <vova234pp@gmail.com>
2025-02-02 20:48:43 +0300

    цмклы

--- 3_урок/main.py
+++ 3_урок/main.py
@@ -4,16 +4,21 @@ add - добавить задачу в список
 show - вывести весь спиок здач"""
 
 tasks = []
+run = True
 
 
-command = input("Введите команду: ")
-if command == "help":
-    print(HELP)
-elif command == "show":
-    print(tasks)
-elif command == "add":
-    task = input("Введе название задачи: ")
-    tasks.append(task)
-    print("Задача добавлена!")
-else:
-    print("Неизвестная команнада! Выберете из доступных:" + HELP)
+while run:
+    command = input("Введите команду: ")
+    if command == "help":
+        print(HELP)
+    elif command == "show":
+        print(tasks)
+    elif command == "add":
+        task = input("Введе название задачи: ")
+        tasks.append(task)
+        print("Задача добавлена!")
+    else:
+        print("Неизвестная команнада! Выберете из доступных:" + HELP)
+        run = False # 1 вариант завершения
+        break
+print("Пока-пока!")
